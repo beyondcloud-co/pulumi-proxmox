7d85ecfcb32de7275cef43718dc46d51314e6c6b
beyondcloud-co <beyondcloud-co@users.noreply.github.com>
2021-10-18 23:56:29 -0400

    Update node package name

--- provider/resources.go
+++ provider/resources.go
@@ -127,6 +127,7 @@ func Provider() tfbridge.ProviderInfo {
 			// "aws_ami": {Tok: makeDataSource(mainMod, "getAmi")},
 		},
 		JavaScript: &tfbridge.JavaScriptInfo{
+			PackageName: "@beyondcloud/proxmox",
 			// List any npm dependencies and their versions
 			Dependencies: map[string]string{
 				"@pulumi/pulumi": "^3.0.0",
